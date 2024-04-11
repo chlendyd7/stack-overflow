3905088dc4cc59f01635d38e07a93a09ab380a3f
Doo Young <88640035+chlendyd7@users.noreply.github.com>
2024-04-11 11:37:07 +0900

    상품 이미지 모델과 migration파일을 만듬

--- project/service/models/product.py
+++ project/service/models/product.py
@@ -5,7 +5,7 @@ class Product(BaseModel):
     
     class Meta:
         verbose_name = '상품'
-        verbose_name_plural = verbose_name+'들'
+        verbose_name_plural = verbose_name
     
     name=models.CharField(
         verbose_name='상품 이름',
@@ -32,6 +32,52 @@ class Product(BaseModel):
     )
     discount=models.PositiveIntegerField(
         verbose_name='할인 금액',
+        null=True,
+        blank=True,
+    )
+
+class ProductImage(BaseModel):
+    class Meta:
+        verbose_name='상품 이미지',
+        verbose_name_plural = verbose_name
+        ordering = ('order',)
+
+    product=models.ForeignKey(
+        to='Product',
+        on_delete=models.CASCADE,
+        related_name='images',
+    )
+    image = models.ImageField(
+        upload_to='product',
+        width_field='width',
+        height_field='height',
+        null=True,
+        blank=True,
+    )
+    image_link = models.URLField(
+        max_length=400,
+        null=True,
+        blank=True,
+    )
+    width = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+    )
+    height = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+    )
+    order = models.PositiveIntegerField(
+        default=1,
+        verbose_name='순서'
+    )
+    start_at = models.DateTimeField(
+        verbose_name='노출 시작 일시',
+        null=True,
+        blank=True,
+    )
+    end_at = models.DateTimeField(
+        verbose_name='노출 종료 일시',
         null=True,
         blank=True,
     )
