e14d1dd0c52cfacde91257b1694f6e7d3c661009
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-04-20 17:21:16 +0900

    라이프 사업단 완료

--- project/htmlwork/urls.py
+++ project/htmlwork/urls.py
@@ -3,7 +3,19 @@ from django.urls import path
 from htmlwork import views
 
 urlpatterns = [
-    path('test', views.life_index),
-    path('sub02', views.life_sub02),
-    path('sub02_02', views.life_sub02_02),
+    path('life_index', views.life_index, name='life_index'),
+    path('life_sub01', views.life_sub01, name='life_sub01'),
+    path('life_sub01_02', views.life_sub01_02, name='life_sub01_02'),
+    path('life_sub01_03', views.life_sub01_03, name='life_sub01_03'),
+    path('life_sub01_04', views.life_sub01_04, name='life_sub01_04'),
+    
+    path('life_sub02', views.life_sub02, name='life_sub02'),
+    path('life_sub02_02', views.life_sub02_02, name='life_sub02_02'),
+    path('life_sub02_03', views.life_sub02_03, name='life_sub02_03'),
+    
+    path('life_sub03', views.life_sub03, name='life_sub03'),
+    
+    path('life_sub04', views.life_sub04, name='life_sub04'),
+    path('life_sub04_02', views.life_sub04_02, name='life_sub04_02'),
+    
 ]
--- project/htmlwork/views.py
+++ project/htmlwork/views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render
 from django.template.loader import render_to_string
 
 # Create your views here.
-def life_index(request):
-    print('test working')
-    return render(request, 'life/index.html')
+def life_index(request): return render(request, 'life/index.html')
 
-def life_sub02(request):
-    return render(request, 'life/sub02.html')
-    # return HttpResponse(render_to_string('life/sub02.html'))
+def life_sub01(request): return render(request, 'life/sub01.html')
+def life_sub01_02(request): return render(request, 'life/sub01_02.html')
+def life_sub01_03(request): return render(request, 'life/sub01_03.html')
+def life_sub01_04(request): return render(request, 'life/sub01_04.html')
 
-def life_sub02_02(request):
-    return render(request, 'life/sub02_02.html')
+def life_sub02(request): return render(request, 'life/sub02.html')
+def life_sub02_02(request): return render(request, 'life/sub02_02.html')
+def life_sub02_03(request): return render(request, 'life/sub02_03.html')
+
+def life_sub03(request): return render(request, 'life/sub03.html')
+def life_sub04(request): return render(request, 'life/sub04.html')
+def life_sub04_02(request): return render(request, 'life/sub04_02.html')
