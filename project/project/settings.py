1a12fbb7336c1d695f553f1bd71ef99853b0ee16
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-04-28 23:50:50 +0900

    24_04_28

--- project/project/settings.py
+++ project/project/settings.py
@@ -173,3 +173,13 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'service.User'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
