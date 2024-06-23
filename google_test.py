88f14e750b0fa649cfb014ff9e353ebc11b4c419
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-06-23 23:44:48 +0900

    240623

--- google_test.py
+++ google_test.py
@@ -7,25 +7,31 @@ from selenium.webdriver.support import expected_conditions as EC
 
 # ChromeDriver 경로 설정
 chromedriver_path = 'C:\Program Files\Google\Chrome\Application\chrome_proxy.exe'
+chromedriver_path = "C:\Program Files\Google\Chrome\Application\chrome_proxy.exe"
+# --profile-directory=Default --app-id=aoeeckacjnficgikkpbofpdcjoohpkld"
 service = Service(chromedriver_path)
+print('work1')
 service = Service(chromedriver_path, log_path='chromedriver.log')
 # ChromeDriver 옵션 설정
 options = webdriver.ChromeOptions()
+print('work2')
 options.add_argument('--start-maximized')  # 브라우저 최대화 옵션
 
 # ChromeDriver 생성
 driver = webdriver.Chrome(service=service, options=options)
-
+print('work3')
 try:
+    print('work1')
     driver.implicitly_wait(10)
+    print('work2')
     # 웹사이트 열기
     driver.get('https://www.google.com')
-
+    print('work3')
     # 검색 창이 로드될 때까지 대기
     search_box = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.NAME, 'q'))
     )
-
+    print('work4')
     # 검색 창에 키워드 입력하고 검색
     search_box.send_keys('Selenium')
     search_box.send_keys(Keys.RETURN)
--- test.py
+++ test.py
@@ -1,33 +1,61 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def auto_create_blog():
-    chrome_options = Options()
-    # chrome_options.add_argument("--headless")  # 브라우저를 열지 않고 실행
-    service = Service('C:\Program Files\Google\Chrome\Application\chrome.exe')  # ChromeDriver 경로 설정
-    options = webdriver.ChromeOptions()
-    options.add_argument('--start-maximized')  # 브라우저 최대화 옵션
+# 크롬 드라이버 생성
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-    # 크롬 드라이버 생성
-    driver = webdriver.Chrome(service=service, options=options)
+# Google 웹사이트에 접속
+driver.get("https://www.tistory.com/auth/login")
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'btn_login')))
+login_button = driver.find_element(By.CLASS_NAME, 'btn_login')
+login_button.click()
+# driver.find_element(By.CLASS_NAME, 'btn_login').click()
+# 검색 입력창 찾기 (검색창의 이름이 'q')
+username = 'chlendyd8@daum.net'
+password = 'd4632077zZ@'
+login_id_element  = driver.find_element(By.ID, 'loginId--1')
+login_id_element .send_keys(username)
+print(login_id_element)
+driver.find_element(By.ID, 'password--2').send_keys(password)
+driver.find_element(By.CLASS_NAME, 'submit').click()
 
-    # 웹사이트 열기
-    driver.get('https://www.google.com')
 
-    # 검색 창에 키워드 입력하고 검색
-    search_box = driver.find_element(By.NAME, 'q')
-    search_box.send_keys('Selenium')
-    search_box.send_keys(Keys.RETURN)
+# link_profile = driver.find_element(By.CSS_SELECTOR, 'a.link_profile')
+# link_profile.click()
+wait = WebDriverWait(driver, 10)
+WebDriverWait(driver, 1000)
 
-    # 검색 결과 출력
-    print(driver.title)
+    # '쓰기' 링크 클릭
+profile_info = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'thumb_profile')))
+print(profile_info)
+# 이 요소 내부에서 '쓰기' 링크 찾기
+thumb_profile = driver.find_element(By.CLASS_NAME, 'thumb_profile')
+thumb_profile.click()
+WebDriverWait(driver, 10)
+link = driver.find_element(By.CSS_SELECTOR, 'a.img_common_tistory.link_edit')
+link.click()
+# '쓰기' 링크 클릭
+print("쓰기 링크를 클릭했습니다.")
 
-    # 브라우저 닫기
-    driver.quit()
 
-auto_create_blog()
+WebDriverWait(driver, 100)
+
+# search_box = driver.find_element(By.ID, 'loginId--1').send_keys(username)
+
+# # 검색어 입력
+# search_box.send_keys('Jennie')
+
+# # 검색 실행
+# search_box.submit()
+# time.sleep(5)
+
+# # 결과 페이지 스크린샷 저장
+# driver.save_screenshot('search_results.png')
+
+# # 브라우저 종료
+# driver.quit()
