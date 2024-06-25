d259b7da1d4b74f867ce29deb0373f4119449f36
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-06-25 23:07:01 +0900

    240625

--- openai_test.py
+++ openai_test.py
@@ -1,27 +1,34 @@
 from openai import OpenAI
+
 client = OpenAI()
 
-def generate_blog_post(topic):
+def generate_blog_post(messages, model="gpt-4o", max_tokens=1000, temperature=0.7, top_p=1.0, n=1):
     try:
-        # 사용자 입력을 prompt로 설정
-        prompt = f"주제: {topic}"
-        
         # OpenAI API를 사용하여 텍스트 생성 요청
         response = client.chat.completions.create(
-            model="gpt-3.5-turbo",  # 적절한 모델 선택
-            prompt=prompt,
-            max_tokens=30,  # 생성할 최대 토큰 수
-            stop=None
+            model=model,
+            messages=messages,
+            max_tokens=max_tokens,
+            temperature=temperature,
+            top_p=top_p,
+            n=n
         )
 
         # 생성된 텍스트 반환
-        return response.choices[0].text.strip()
+        print(response)
+        return response.choices[0].message.content.strip()
 
     except Exception as e:
         print(f"Error: {e}")
         return None
-topic = 'chat gpt로 자동화 블로그 글을 썻다는 내용'
-generated_text = generate_blog_post(topic)
+
+messages = [
+    {"role": "user", "content": "백엔드 기술 중 하나에 대해 글을 써줘 퀄리티는 2년차 개발자가 쓴 것 처럼"},
+    {"role": "user", "content": "블로그에 올릴 수 있을 정도로 사람이 쓴 것 처럼"},
+    {"role": "user", "content": "소개 글 빼고 마크다운 형식으로 문단을 잘 구분해서"}
+]
+generated_text = generate_blog_post(messages)
+
 if generated_text:
     print("생성된 블로그 글:")
     print(generated_text)
