5a468b08f4b14a15ba7eb1d4c1751b984a6c4317
Doo Young <88640035+chlendyd7@users.noreply.github.com>
2024-07-15 18:32:43 +0900

    코드 리팩토링

--- macro/main.py
+++ macro/main.py
@@ -1,20 +1,34 @@
 from excel import open_excel
 from naver import Naver
-from tqdm import tqdm
+import datetime
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+
+
+def get_driver():
+    service = Service(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    # options.add_argument("--headless")
+    driver = webdriver.Chrome(service=service, options=options)
+
+    return driver
 
 def main():
-    try:
-        url_list, id_list = open_excel()
-        for id, pw in tqdm(id_list, desc="Processing"):
-            print()
-            print(f'{id} working')
-            if id == None:
-                continue
-            Naver(url_list).start(id, pw)
-            input("종료 하시려면 엔터를 누르세요")
-    except Exception as e:
-        print(f"오류 발생: {e}")
-        input("종료 하시려면 엔터를 누르세요")  # 사용자가 엔터 키를 누를 때까지 대기
+    test_date_end = datetime.datetime(2024, 7, 20)
+    now = datetime.datetime.now()
+    if test_date_end > now:
+        try:
+            driver = get_driver()
+            url_list, id_list = open_excel()
+            naver = Naver(driver, url_list, id_list)
+            naver.start()
+            input("모두 완료했습니다 종료 하시려면 엔터를 누르세요")
+        except Exception as e:
+            print(f"오류 발생: {e}")
+            input("종료 하시려면 엔터를 누르세요")  # 사용자가 엔터 키를 누를 때까지 대기
+    else:
+        print('테스트 기간이 경과 되었습니다. 개발자에게 문의 해주세요')
 
 if __name__ == '__main__':
     main()
--- macro/naver.py
+++ macro/naver.py
@@ -1,17 +1,12 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
-import openpyxl
+from tqdm import tqdm
 
 def find_css_element_safe(driver, value):
     try:
@@ -19,40 +14,41 @@ def find_css_element_safe(driver, value):
                 EC.presence_of_element_located((By.CSS_SELECTOR, value)))
         return element
     except NoSuchElementException:
-        return None 
+        return None
 
 
 class Naver:
 
-    def __init__(self, urls):
+    def __init__(self, driver, urls, ids):
+        self.driver = driver
         self.urls = urls
+        self.ids = ids
 
-    def start(self, id, pw):
-        try:
-            service = Service(ChromeDriverManager().install())
-            options = webdriver.ChromeOptions()
-            # options.add_argument("--headless")
-            driver = webdriver.Chrome(service=service, options=options)
-
-            url = 'https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/'
-            driver.get(url)
-            id = id
-            password = pw
-
-            driver.execute_script("document.getElementById('id').value = arguments[0];", id)
-            driver.execute_script("document.getElementById('pw').value = arguments[0];", password)
-
-            check_butoon = find_css_element_safe(driver, '.switch_btn')
-            check_butoon.click()
-            login_button = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, '#log\.login')))
-            login_button.click()
-
+    def start(self):
+        for id, pw in tqdm(self.ids, desc="Processing"):
+            if id == None:
+                continue
+            print(f'{id} processing')
+            self.naver_login(self.driver, id, pw)
             for url in self.urls:
                 if type(url) != str:
                     continue
-                time.sleep(2)
-                self.click_sympathy(driver, url)
+                self.click_sympathy(self.driver, url)
+
+    def naver_login(self, driver, id, pw):
+        try:
+            url = 'https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/'
+            driver.get(url)
+            driver.execute_script("document.getElementById('id').value = arguments[0];", id)
+            driver.execute_script("document.getElementById('pw').value = arguments[0];", pw)
+
+            check_button = find_css_element_safe(driver, '.switch_btn')
+            check_button.click()
+            login_button = find_css_element_safe(driver, '#log\.login')
+            if login_button:
+                login_button.click()
+            time.sleep(2)
+            # 로그인 성공 로직 넣기
 
         except Exception as e:
             print(f"로그인 실패: id:{id}, pw:{pw}")
@@ -83,8 +79,8 @@ class Naver:
             # )
 
             sympathy_element.click()
-            time.sleep(2)
             error_check_blog_name = find_css_element_safe(driver, '#blogDomainTitle')
+            time.sleep(2)
             if error_check_blog_name:
                 return print('공감을 남기려면 블로그 아이디가 필요해요')
             else:
